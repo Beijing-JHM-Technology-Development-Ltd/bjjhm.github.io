f3485d157223bf1d286ae808643ce6b6f8a1b77c
lozia <lizongyao9745@hotmail.com>
2023-07-03 23:37:39 +0800

    slide rotating effect finished.

---
+++ scripts/exhibition.js
@@ -0,0 +1,24 @@
+console.log(`Script loaded.`)
+
+let box = document.getElementById('slide');
+let imgs = document.getElementById('imgs');
+let img2 = document.getElementById('imgs2');
+
+let left = 0;
+let left2 = 0;
+
+imgs2.innerHTML = imgs.innerHTML;
+
+setInterval(() => {
+    left -= 360;
+    left2 -= 360;
+
+    imgs.style.left = left+'px';
+    imgs2.style.left = left2+'px';
+    if (left *-1 > imgs.clientWidth){
+        left = imgs.clientWidth;
+    }
+    if (left2 *-1 > 2*imgs.clientWidth){
+        left2 = 0;
+    }
+},2000);
