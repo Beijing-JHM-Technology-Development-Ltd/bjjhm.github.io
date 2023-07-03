3b13c7f0bf22529044c33b3964d2e4f40b6619dc
lozia <lizongyao9745@hotmail.com>
2023-07-03 23:59:18 +0800

    slide rotating effect finished.

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,24 +1,35 @@
 console.log(`Script loaded.`)
 
-let box = document.getElementById('slide');
-let imgs = document.getElementById('imgs');
-let img2 = document.getElementById('imgs2');
+const box = document.getElementById('slide');
+const imgs = document.getElementById('imgs');
+const img2 = document.getElementById('imgs2');
+
 
 let left = 0;
 let left2 = 0;
 
 imgs2.innerHTML = imgs.innerHTML;
 
-setInterval(() => {
-    left2 -= 1;
-    left -= 1;
 
-    imgs.style.left = left+'px';
-    imgs2.style.left = left2+'px';
-    if (left *-1 > imgs.clientWidth){
-        left = imgs.clientWidth;
-    }
-    if (left2 *-1 > 2*imgs.clientWidth){
-        left2 = 0;
-    }
-},16);
+let playId;
+let move = ()=> { 
+    playId = setInterval(() => {
+        left2 -= 1;
+        left -= 1;
+
+        imgs.style.left = left+'px';
+        imgs2.style.left = left2+'px';
+        if (left *-1 > imgs.clientWidth){
+            left = imgs.clientWidth;
+        }
+        if (left2 *-1 > 2*imgs.clientWidth){
+            left2 = 0;
+        }
+    },16);
+}
+move();
+// console.log(`${box}`)
+// box.onmouseenter((e)=>clearInterval(playId));
+
+box.addEventListener('mouseenter', (e) => clearInterval(playId));
+box.addEventListener('mouseleave', (e) => move());
