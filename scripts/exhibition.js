3c0335052ba287cea430559cac2c86b7e094e304
lozia <lizongyao9745@hotmail.com>
2023-07-04 11:18:07 +0800

    Modified exhibition rule

--- scripts/exhibition.js
+++ scripts/exhibition.js
@@ -1,14 +1,13 @@
 console.log(`Script loaded.`)
 
-const box = document.getElementById('slide');
+const box = document.getElementById('exhibition');
 const imgs = document.getElementById('imgs');
-const img2 = document.getElementById('imgs2');
 
 
-let left = 0;
-let left2 = 0;
-
-imgs2.innerHTML = imgs.innerHTML;
+let left = imgs.clientWidth/3+1;
+// let left2 = 0;
+// const img2 = document.getElementById('imgs2');
+// imgs2.innerHTML = imgs.innerHTML;
 
 
 let playId;
@@ -19,15 +18,15 @@ let move = ()=> {
         imgs.style.left = left+'px';
 
         if (left *-1 > imgs.clientWidth){
-            left = imgs.clientWidth;
+            left = imgs.clientWidth/3+1;
         }
 
-        left2 -= imgs.clientWidth/3+1;
-        imgs2.style.left = left2+'px';
-        if (left2 *-1 > 2*imgs.clientWidth){
-            left2 = 0;
-        }
-    },2000);
+        // left2 -= imgs.clientWidth/3+1;
+        // imgs2.style.left = left2+'px';
+        // if (left2 *-1 > 2*imgs.clientWidth){
+        //     left2 = 0;
+        // }
+    },2500);
 }
 move();
 
